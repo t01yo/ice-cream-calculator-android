ab8e6b9dd5f202393a717fc406309e5bc150878a
bitress <cyannejustinl.vega12@gmail.com>
2023-10-27 19:59:04 +0800

    fixed activity_home.xml

--- app/src/main/java/com/bitress/icecreamcalculator/HomeActivity.kt
+++ app/src/main/java/com/bitress/icecreamcalculator/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.bitress.icecreamcalculator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
@@ -12,10 +13,43 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        val mangoFlavourButton = findViewById<Button>(R.id.mangoButton)
+        val mintFlavourButton = findViewById<Button>(R.id.mintButton)
+        val vanillaFlavourButton = findViewById<Button>(R.id.vanillaButton)
+
+        val mangoIndicator = findViewById<View>(R.id.mangoIndicator)
+        val mintIndicator = findViewById<View>(R.id.mintIndicator)
+        val vanillaIndicator = findViewById<View>(R.id.vanillaIndicator)
+
+
+        val flavours = listOf(mangoFlavourButton, mintFlavourButton, vanillaFlavourButton)
+        val indicators = listOf(mangoIndicator, mintIndicator, vanillaIndicator)
+
+        var selectedFlavour = ""
+        val selectedSize = ""
+
+        for ((index, flavour) in flavours.withIndex()) {
+            flavour.setOnClickListener {
+                for (indicator in indicators) {
+                    indicator.visibility = View.GONE
+                }
+                indicators[index].visibility = View.VISIBLE
+                for (btn in flavours) {
+                    btn.isSelected = false
+                }
+                it.isSelected = true
+                selectedFlavour = (it as Button).text.toString()
+            }
+        }
+
+
         placeOrderButton = findViewById(R.id.placeOrderButton)
 
         placeOrderButton.setOnClickListener {
+
             val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("flavour", selectedFlavour)
+
             startActivity(intent)
         }
 
--- app/src/main/java/com/bitress/icecreamcalculator/SecondActivity.kt
+++ app/src/main/java/com/bitress/icecreamcalculator/SecondActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
 
@@ -12,8 +13,14 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
+        val flavorTextView = findViewById<TextView>(R.id.flavorTextView)
+
         returnHomeButton = findViewById(R.id.returnHomeButton)
 
+
+
+        val flavor = intent.getStringExtra("flavour")
+        flavorTextView.text = "Flavor: $flavor"
 
         // Intent Return to Home Activity
         returnHomeButton.setOnClickListener {
